6b5f84279598ed9d56b016719f62f0d029e4feea
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2018-05-10 21:39:32 +0200

    slider positioning
    - slider is placed into container center, shaped either vertical or horizontal depending on container height/width
    - added sliders' handles in default position

--- js/main.js
+++ js/main.js
@@ -2,44 +2,107 @@
 class Slider {
   constructor(options) {
       this._options = options;
-      // get canvas from HTML
-      this.ctx = document.getElementById(this._options.containerId).getContext('2d');
-      // set canvas scale
-      this.ctx.canvas.width = this._options.width;
-      this.ctx.canvas.height = this._options.height;
-      // call method to draw empty sliders
+      this.ctx;
+      // call method to draw sliders
+      this.createCanvas();
       this.drawSliders();
+      this.drawHandles();
   }
 
-  // draw empty sliders defined in options parameter
+  createCanvas() {
+    // create canvas and append it to container
+    const container = document.getElementById(this._options.container);
+    let contW = container.getBoundingClientRect().width;
+    let contH = container.getBoundingClientRect().height;
+    const canvas = document.createElement('canvas');
+    canvas.id = 'canvas';
+    if (contW > contH) {
+      canvas.height = contH * .95;
+      canvas.width = canvas.height * 1.5;
+    }
+    else {
+      canvas.width = contW * .95;
+      canvas.height = canvas.width * 1.5;
+    }
+
+    canvas.style.position = 'relative';
+    canvas.style.top = '50%';
+    canvas.style.left = '50%';
+    canvas.style.transform = 'translate(-50%, -50%)';
+    canvas.style.border = '5px dashed red';
+    container.appendChild(canvas);
+    this.ctx = canvas.getContext('2d');
+  }
+
+  // draw empty sliders
   drawSliders() {
-    let r;
+    const ctx = this.ctx;
+    const cw = ctx.canvas.width;
+    const ch = ctx.canvas.height;
     const pi = Math.PI;
-    const cw = this.ctx.canvas.width;
-    const ch = this.ctx.canvas.height;
-    this.ctx.lineWidth = 15;
-    this.ctx.strokeStyle = '#eee';
+    ctx.lineWidth = 15;
+    ctx.strokeStyle = '#eee';
+    let r;
+    // sliders center position (right or bottom)
+    let center = (canvas.width > canvas.height) ? {x: cw*2/3, y: ch/2} : {x: cw/2, y: ch*2/3};
+    // draw sliders
     for (let i = 0; i < this._options.sliders.length; i++) {
-      // define radius
       r = this._options.sliders[i].radius;
-      // draw empty slider
-      this.ctx.beginPath();
-      this.ctx.arc(cw*2/3, ch/2, r, 0, 2*pi, false);
-      this.ctx.stroke();
+      ctx.beginPath();
+      ctx.arc(center.x, center.y, r, 0, 2*pi, false);
+      ctx.stroke();
     }
   }
+
+  // draw handles with default positions
+  drawHandles() {
+    const ctx = this.ctx;
+    const cw = ctx.canvas.width;
+    const ch = ctx.canvas.height;
+    const pi = Math.PI;
+    // handle radius
+    const r = 10;
+    // sliders center position (right or bottom)
+    let center = (canvas.width > canvas.height) ? {x: cw*2/3, y: ch/2} : {x: cw/2, y: ch*2/3};
+    // handle coordinates
+    let x = center.x;
+    let y;
+    // draw handles
+    for (let i = 0; i < this._options.sliders.length; i++) {
+      // y coordinate of handle
+      y = center.y - this._options.sliders[i].radius;
+      ctx.beginPath();
+      ctx.arc(x, y, r, 0, 2*pi, false);
+      ctx.fillStyle = "#eeefef";
+      ctx.fill();
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = '#cfcfcf';
+      ctx.stroke();
+    }
+  }
+
+  animateHandles() {
+    // if clicked on radius of one of the sliders, redraw handle on that position
+
+  }
+
+
+
+  // // draw values on the left
+  // const value = 50;  // to be added
+  // ctx.textAlign = 'left';
+  // const fontSize = cw * 0.05 + 'px';
+  // ctx.font="fontSize Arial";
+  // ctx.fillText('$'+value, cw/5, ch*0.4);
+
 };
 
 // enter options
 const options = {
-  containerId: 'canvas',
-  width: 600,
-  height: 400,
+  container: 'container',
   sliders: [
     {radius: 80},
-    {radius: 150},
-    {radius: 120},
-    {radius: 30}]
+    {radius: 120}]
 };
 
 // initialize slider object with options defined above
