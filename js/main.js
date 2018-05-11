cb308fcc73414b3eb36d8b3d9cd9f65678cc4da3
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2018-05-11 02:10:23 +0200

    slider handles move on click
    - slider handles and change position if user clicks on slider

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ class Slider {
       this.makeSliders();
   }
 
+
   // create canvas and append it to DOM container defined in options parameter
   createCanvas() {
     const container = document.getElementById(this._options.container);
@@ -36,14 +37,16 @@ class Slider {
     this.ctx = canvas.getContext('2d');
   }
 
+
+  // draw sliders, handles & data field
   makeSliders() {
-    // variables
+    // common variables
     const options = this._options;
     const ctx = this.ctx;
     const cw = ctx.canvas.width;
     const ch = ctx.canvas.height;
     const pi = Math.PI;
-    // sliders' center position (right or bottom)
+    // sliders center position
     let center = (canvas.width > canvas.height) ? {x: cw*2/3, y: ch/2} : {x: cw/2, y: ch*2/3};
 
     // draw empty sliders
@@ -59,37 +62,71 @@ class Slider {
       }
     }
 
-    // draw handles with default positions
+
+    // array of handles with coordinates that can be changed with user input (click, drag... )
+    let handles = [];
+    for (let i = 0; i < options.sliders.length; i++) {
+      handles.push({r: options.sliders[i].radius, x: center.x, y: center.y - options.sliders[i].radius});
+    }
+
+
+    // draw handles
     function drawHandles() {
-      // handle radius
       const r = 10;
-      // handle coordinates
-      let x = center.x;
-      let y;
       // draw handles
-      for (let i = 0; i < options.sliders.length; i++) {
-        y = center.y - options.sliders[i].radius;
+      handles.forEach(function(handle) {
         ctx.beginPath();
-        ctx.arc(x, y, r, 0, 2*pi, false);
+        ctx.arc(handle.x, handle.y, r, 0, 2*pi, false);
         ctx.fillStyle = "#eeefef";
         ctx.fill();
         ctx.lineWidth = 1;
         ctx.strokeStyle = '#cfcfcf';
         ctx.stroke();
-      }
+      });
     }
 
+
+    // animate handles
     function animateHandles() {
-      // if clicked on radius of one of the sliders, redraw handle on that position
+      let mouseX, mouseY;
+      let x, y, z;
+
+      // move handle if clicked on a slider
+      ctx.canvas.addEventListener('click', function(event) {
+        // mouse coordinates inside canvas
+        mouseX = event.clientX - canvas.getBoundingClientRect().left;
+        mouseY = event.clientY - canvas.getBoundingClientRect().top;
+        // x & y distance from slider center to mouse
+        x = mouseX - center.x;
+        y = mouseY - center.y;
+        // absolute distance from slider center to mouse
+        z = (x**2 + y**2)**0.5;
+
+        // draw handles
+        handles.forEach(function(handle) {
+          // if a slider was clicked, adjust position of its handle
+          if (z < handle.r + 10 && z > handle.r - 10) {
+            handle.x = handle.r/z * x + center.x;
+            handle.y = handle.r/z * y + center.y;
+            // clear canvas
+            ctx.clearRect(0, 0, cw, ch);
+            // draw empty sliders
+            drawSliders();
+            // draw all handles
+            drawHandles();
+          }
+        });
+      });
     }
 
+
+    // draw empty sliders
     drawSliders();
+    // draw handles
     drawHandles();
+    // listen for user input and adjust handles
     animateHandles();
   }
-
-
-
 
   // // draw values on the left
   // const value = 50;  // to be added
@@ -104,13 +141,13 @@ class Slider {
 const options = {
   container: 'container',
   sliders: [
-    {radius: 80},
-    {radius: 120}]
+    {radius: 50, color: 'red'},
+    {radius: 80, color: 'green'},
+    {radius: 110, color: 'blue'}]
 };
 
 // initialize slider object with options defined above
 const slider = new Slider(options);
-
 
 
 // options:
