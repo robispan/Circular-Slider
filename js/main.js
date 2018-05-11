5391d8525371c12d6b5d80b4b629715c40d85a7f
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2018-05-11 22:19:07 +0200

    drag handles
    - handles move position on dragging

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ class Slider {
       this.ctx;
       // call methods to create canvas and sliders
       this.createCanvas();
-      this.makeSliders();
+      this.drawObjects();
   }
-
 
   // create canvas and append it to DOM container defined in options parameter
   createCanvas() {
@@ -37,10 +36,9 @@ class Slider {
     this.ctx = canvas.getContext('2d');
   }
 
-
-  // draw sliders, handles & data field
-  makeSliders() {
-    // common variables
+  // draw sliders, handles & data fields, move sliders
+  drawObjects() {
+    // global variables
     const options = this._options;
     const ctx = this.ctx;
     const cw = ctx.canvas.width;
@@ -49,7 +47,7 @@ class Slider {
     // sliders center position
     let center = (canvas.width > canvas.height) ? {x: cw*2/3, y: ch/2} : {x: cw/2, y: ch*2/3};
 
-    // draw empty sliders
+    // draw sliders
     function drawSliders() {
       ctx.lineWidth = 15;
       ctx.strokeStyle = '#eee';
@@ -62,18 +60,15 @@ class Slider {
       }
     }
 
-
-    // array of handles with coordinates that can be changed with user input (click, drag... )
-    let handles = [];
+    // make array of handles
+    const handles = [];
     for (let i = 0; i < options.sliders.length; i++) {
       handles.push({r: options.sliders[i].radius, x: center.x, y: center.y - options.sliders[i].radius});
     }
 
-
     // draw handles
     function drawHandles() {
       const r = 10;
-      // draw handles
       handles.forEach(function(handle) {
         ctx.beginPath();
         ctx.arc(handle.x, handle.y, r, 0, 2*pi, false);
@@ -85,29 +80,35 @@ class Slider {
       });
     }
 
-
-    // animate handles
-    function animateHandles() {
+    // move handle
+    function moveHandle() {
       let mouseX, mouseY;
       let x, y, z;
+      let mouseDown;
+      console.log('moveHandles!');
+      // listen to mouse up
+      ctx.canvas.addEventListener('mouseup', function() {
+        mouseDown = false;
+        console.log('mouseup!');
+      });
 
-      // move handle if clicked on a slider
-      ctx.canvas.addEventListener('click', function(event) {
-        // mouse coordinates inside canvas
-        mouseX = event.clientX - canvas.getBoundingClientRect().left;
-        mouseY = event.clientY - canvas.getBoundingClientRect().top;
-        // x & y distance from slider center to mouse
-        x = mouseX - center.x;
-        y = mouseY - center.y;
-        // absolute distance from slider center to mouse
-        z = (x**2 + y**2)**0.5;
-
-        // draw handles
+      function redrawHandles(z) {
         handles.forEach(function(handle) {
-          // if a slider was clicked, adjust position of its handle
-          if (z < handle.r + 10 && z > handle.r - 10) {
-            handle.x = handle.r/z * x + center.x;
-            handle.y = handle.r/z * y + center.y;
+          let z1;
+          // check if slider was clicked
+          if (z < handle.r + 10 && z > handle.r - 10 && mouseDown) {
+            // get mouse coordinates inside canvas
+            mouseX = event.clientX - canvas.getBoundingClientRect().left;
+            mouseY = event.clientY - canvas.getBoundingClientRect().top;
+            // get x & y distance from slider center to mouse
+            x = mouseX - center.x;
+            y = mouseY - center.y;
+            // get absolute distance from slider center to mouse
+            z1 = (x**2 + y**2)**0.5;
+            console.log('z1:' + z1);
+            // draw handles
+            handle.x = handle.r/z1 * x + center.x;
+            handle.y = handle.r/z1 * y + center.y;
             // clear canvas
             ctx.clearRect(0, 0, cw, ch);
             // draw empty sliders
@@ -116,16 +117,38 @@ class Slider {
             drawHandles();
           }
         });
-      });
-    }
+      }
 
+      // move handle on mouse down
+      ctx.canvas.addEventListener('mousedown', function() {
+        mouseDown = true;
+        console.log('mousedown!');
+        // mouse coordinates inside canvas
+        mouseX = event.clientX - canvas.getBoundingClientRect().left;
+        mouseY = event.clientY - canvas.getBoundingClientRect().top;
+        // x & y distance from slider center to mouse
+        x = mouseX - center.x;
+        y = mouseY - center.y;
+        // absolute distance from slider center to mouse
+        z = (x**2 + y**2)**0.5;
+        console.log('z:' + z);
+
+        redrawHandles(z);
+      });
+
+      // drag handle on mouse down
+      ctx.canvas.addEventListener("mousemove", function() {
+        redrawHandles(z);
+      });
+
+    }
 
     // draw empty sliders
     drawSliders();
     // draw handles
     drawHandles();
     // listen for user input and adjust handles
-    animateHandles();
+    moveHandle();
   }
 
   // // draw values on the left
@@ -142,6 +165,7 @@ const options = {
   container: 'container',
   sliders: [
     {radius: 50, color: 'red'},
+    {radius: 150, color: 'red'},
     {radius: 80, color: 'green'},
     {radius: 110, color: 'blue'}]
 };
