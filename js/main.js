b9d7f44ffc0deb7c9c6156dd8cc5d476911754c3
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2018-05-13 03:02:21 +0200

    added steps
    - corrected calculations of values
    - incorporated step values to data output and slider paths

--- js/main.js
+++ js/main.js
@@ -61,59 +61,80 @@ class Slider {
       });
     }
 
-    // get user value from radians
+    // get user value from radians (correction of 1.013 is needed)
     function getValue(diff, max, min) {
       if (diff + pi/2 < 0) {
-        return (2.5*pi + diff) * (max-min) / 20*pi + min;
+        return ((diff + pi*2.5) * (max-min) / 20*pi) * 1.013 + min;
       } else {
-        return (diff + pi/2) * (max-min) / 20*pi + min;
+        return ((diff + pi/2) * (max-min) / 20*pi) * 1.013 + min;
       }
     }
 
-    const r = 5;  // handle radius
+    // floor angle to lower step
+    let parts;
+    function floorAngle(diff, max, min, step) {
+      parts = (max - min) / step;
+      return Math.floor((diff + 0.5*pi) / (2*pi/parts)) * (2*pi/parts) - 0.5*pi;
+    }
+
+    const r = 12;  // handle radius
     let diff;  // distance from slider top to handle in radians
+    let diff2;  // diff for paths
     let value;  // slider value in user units
     let max;  // user defined max value
     let fontSize;  // data font size
-    let bigDash;  // dashes on slider
-    let smallDash; // transparent dashes on slider
+    let my_gradient;  // handle gradient
+    // let bigDash;  // dashes on slider
+    // let smallDash; // transparent dashes on slider
     // data position
-    const dataPosition = (canvas.width > canvas.height) ? {x: cw*0.2, y: ch/3} : {x: cw/4, y: ch*0.1};
+    const dataPosition = (canvas.width > canvas.height) ? {x: cw*0.03, y: ch*0.33} : {x: cw*0.03, y: ch*0.07};
 
     // draw sliders, handles & colored paths
     function drawsliders() {
       sliders.forEach(function(slider, index) {
         // draw sliders backgrounds
-        bigDash = ((pi*2*slider.r/(slider.max - slider.min)) * 1.0001) - 2;
-        smallDash = 2;
-        ctx.setLineDash([bigDash, smallDash]);
-        ctx.lineWidth = 15;
-        ctx.strokeStyle = '#aaa';
+        // bigDash = slider.step * ((pi*2*slider.r/(slider.max - slider.min)) * 1.0001) - 2;  // correction for factor 1.0001
+        // smallDash = 2;
+        ctx.setLineDash([6, 1]);
+        ctx.lineWidth = 20;
+        ctx.strokeStyle = '#ddd';
         ctx.beginPath();
         ctx.arc(center.x, center.y, slider.r, -.5*pi, 1.5*pi, false);
         ctx.stroke();
         // draw paths
-        diff = Math.atan2(slider.y - center.y, slider.x - center.x);
+        diff = Math.atan2(slider.y - center.y, slider.x - center.x);  // path end
+        diff2 = floorAngle(diff, slider.max, slider.min, slider.step);  // path end floored to nearest step
+        // background path
+        ctx.setLineDash([]);
+        ctx.globalAlpha = 0.7;
         ctx.beginPath();
-        ctx.arc(center.x, center.y, slider.r, 1.5*pi, diff + 0.00001, false);
-        ctx.lineWidth = 15;
+        ctx.arc(center.x, center.y, slider.r, -.5*pi, diff2 + 0.00001, false);  // correction of 0.00001
+        ctx.strokeStyle = slider.color;
+        ctx.stroke();
+        // colored path
+        ctx.setLineDash([6, 1]);
+        ctx.globalAlpha = 1;
+        ctx.beginPath();
+        ctx.arc(center.x, center.y, slider.r, -.5*pi, diff2 + 0.00001, false);  // correction of 0.00001
         ctx.strokeStyle = slider.color;
         ctx.stroke();
         // draw handles
+        ctx.setLineDash([]);
+        ctx.fillStyle = '#f2f2f2';
         ctx.beginPath();
         ctx.arc(slider.x, slider.y, r, 0, 2*pi, false);
-        ctx.fillStyle = "#eeefef";
         ctx.fill();
         ctx.lineWidth = 1;
-        ctx.strokeStyle = '#cfcfcf';
+        ctx.strokeStyle = '#c7c7c7';
         ctx.stroke();
         // draw data
-        value = Math.floor(getValue(diff, slider.max, slider.min) * 1.013);
-        fontSize = cw * 0.03;
-        ctx.font = fontSize + "px Arial";
-        ctx.fillStyle = "red";
+        value = getValue(diff, slider.max, slider.min);
+        value = Math.floor(value / slider.step) * slider.step;  // floor value to steps
+        fontSize = (canvas.width > canvas.height) ? cw * 0.07 : ch * 0.07;
+        ctx.font = 'bold ' + fontSize + 'px Calibri';
+        ctx.fillStyle = "#111";
         ctx.textAlign = 'left';
-        ctx.fillText('$' + value, dataPosition.x, dataPosition.y + index*cw/20);
+        ctx.fillText('$' + value, dataPosition.x, dataPosition.y + index*fontSize*0.9);
       });
     }
 
@@ -122,11 +143,9 @@ class Slider {
       let mouseX, mouseY;
       let x, y, z, z1;
       let mouseDown;
-      console.log('moveHandles!');
       // listen to mouse up
       ctx.canvas.addEventListener('mouseup', function() {
         mouseDown = false;
-        console.log('mouseup!');
       });
 
       function redrawsliders(z) {
@@ -141,7 +160,6 @@ class Slider {
             y = mouseY - center.y;
             // get absolute distance from slider center to mouse
             z1 = (x**2 + y**2)**0.5;
-            console.log('z1:' + z1);
             // adjust handle position
             handle.x = handle.r/z1 * x + center.x;
             handle.y = handle.r/z1 * y + center.y;
@@ -156,7 +174,6 @@ class Slider {
       // move handle on mouse down
       ctx.canvas.addEventListener('mousedown', function() {
         mouseDown = true;
-        console.log('mousedown!');
         // mouse coordinates inside canvas
         mouseX = event.clientX - canvas.getBoundingClientRect().left;
         mouseY = event.clientY - canvas.getBoundingClientRect().top;
@@ -165,7 +182,6 @@ class Slider {
         y = mouseY - center.y;
         // absolute distance from slider center to mouse
         z = (x**2 + y**2)**0.5;
-        console.log('z:' + z);
 
         redrawsliders(z);
 
@@ -187,7 +203,6 @@ class Slider {
     document.addEventListener("touchmove", touch2Mouse, true);
     document.addEventListener("touchend", touch2Mouse, true);
     function touch2Mouse(e) {
-      console.log('touchtomouse');
       var theTouch = e.changedTouches[0];
       var mouseEv;
       switch(e.type) {
@@ -214,12 +229,18 @@ class Slider {
 const options = {
   container: 'container',
   sliders: [
-    {radius: 100, color: 'teal', max: 20, min: 5, step: 1},
-    {radius: 150, color: 'red', max: 20, min: 10, step: 2}]
+    {radius: 40, color: 'red', max: 1000, min: 0, step: 10},
+    {radius: 70, color: '#f3771c', max: 1000, min: 10, step: 2},
+    {radius: 100, color: '#009b19', max: 1000, min: 20, step: 20},
+    {radius: 130, color: '#0080bb', max: 1000, min: 30, step: 2},
+    {radius: 160, color: '#6a427c', max: 1000, min: 35, step: 5}
+  ]
 };
 
 // initialize slider object with options object
 const slider = new Slider(options);
+
+
 
 
 
